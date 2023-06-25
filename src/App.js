1f2cbae7a3906bb086ddf6a957c6eaa6c552c035
Legas <00legas00@gmail.com>
2023-06-25 16:41:47 +0300

    Создал скелет будущего приложения

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img className='logo' src="https://www.freelogodesign.org/assets/img/home/icones/globe.svg" alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img className='image'
+                         src="https://avatars.mds.yandex.net/i?id=71e71b6b422ee1cf21412fc9b34193a04b37b56f-8497411-images-thumbs&ref=rim&n=33&w=480&h=150"
+                         alt=""/>
+                </div>
+
+                <div>
+                    ava + description
+                </div>
+
+                <div>
+                    my posts
+                    <div>new post</div>
+                    <div>
+                        <div>Post 1</div>
+                        <div>Post 2</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
